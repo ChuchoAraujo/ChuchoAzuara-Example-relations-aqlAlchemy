f4e6045168550e78ea6924f5d6aae6e03886df9f
JesusAraujo <jeseazuara@gmail.com>
2023-05-15 17:39:32 +0000

    ok

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,16 +36,199 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#########################################################  USERS #############################################################################################
+
+#------------------------------- GET USERS -------------------------------
+@app.route('/users', methods=['GET'])
+def get_all_users():
+
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+
+    return jsonify(serialized_users), 200
+
+#------------------------------- GET USER/ID -------------------------------
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+
+    user = User.query.get(user_id)
+    serialized_user = user.serialize()
+
+    return jsonify(serialized_user), 200
+
+#------------------------------- POST USERS -------------------------------
+@app.route('/users', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    new_user = User(email = body['email'], password = body['password'])
+    db.session.add(new_user)
+    db.session.commit()
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "User created successfully", 
+        "user": user.serialize()
     }
 
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+#------------------------------- DELETE USERS -------------------------------
+
+@app.route('/users/<int:user_id>', methods= ['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    response_body = {
+        "msg": "User deleted", 
+        "user": user.serialize()
+    }
+
+    return jsonify(response_body)
+
+
+#########################################################  PEOPLE #############################################################################################
+
+#------------------------------- GET PEOPLE -------------------------------
+@app.route('/people', methods=['GET'])
+def get_all_people():
+
+    people = People.query.all()
+    serialized_people = [p.serialize() for p in people]
+
+    return jsonify(serialized_people), 200
+
+
+#------------------------------- GET USER/ID -------------------------------
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people(people_id):
+
+    people = People.query.get(people_id)
+    serialized_people = people.serialize()
+
+    return jsonify(serialized_people), 200
+
+#------------------------------- POST PEOPLE -------------------------------
+@app.route('/people', methods=['POST'])
+def create_people():
+    body = request.get_json()
+    new_people = People(name = body['name'], description = body['description'])
+    db.session.add(new_people)
+    db.session.commit()
+
+    response_body = {
+        "msg": "People created successfully", 
+        "people": new_people.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+#------------------------------- DELETE USERS -------------------------------
+
+@app.route('/people/<int:people_id>', methods= ['DELETE'])
+def delete_people(people_id):
+    people = People.query.get(people_id)
+    db.session.delete(people)
+    db.session.commit()
+
+    response_body = {
+        "msg": "people deleted", 
+        "people": people.serialize()
+    }
+
+    return jsonify(response_body)
+
+
+#########################################################  PLANETS#############################################################################################
+
+#------------------------------- GET PLANETS -------------------------------
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+
+    planets = Planet.query.all()
+    serialized_planets = [p.serialize() for p in planets]
+
+    return jsonify(serialized_planets), 200
+
+
+#------------------------------- GET PLANET/ID -------------------------------
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+
+    planet = Planet.query.get(planet_id)
+    serialized_planet = planet.serialize()
+
+    return jsonify(serialized_planet), 200
+
+#------------------------------- POST PEOPLE -------------------------------
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    body = request.get_json()
+    new_planet = Planet(name = body['name'], climate = body['climate'], terrain = body['terrain'])
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Planet created successfully", 
+        "people": new_planet.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+#------------------------------- DELETE USERS -------------------------------
+
+@app.route('/people/<int:planet_id>', methods= ['DELETE'])
+def delete_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    response_body = {
+        "msg": "planet deleted", 
+        "people": planet.serialize()
+    }
+
+    return jsonify(response_body)
+
+
+
+#########################################################  FAVORITES #############################################################################################
+
+#------------------------------- POST FAV PEOPLE -------------------------------
+
+@app.route('/users/<int:user_id>/people', methods=['POST'])
+def add_favorite_people(user_id):
+    # Capturamos la informacion del request body y accedemos al people id
+    body = request.get_json()
+    people_id = body['people_id']
+    
+    user = User.query.get(user_id)
+    people = People.query.get(people_id)
+
+    new_favorite = Favorite(user=user, people=people)
+    db.session.add(new_favorite)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Favorito agregado correctamente", 
+        "favorite": new_favorite.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+#------------------------------- GET USER ID FAVORITE -------------------------------
+
+@app.route('/users/<int:user_id>/favorites', methods=['GET'])
+def get_user_favorites(user_id):
+    user = User.query.get(user_id)
+    favorites = user.favorites
+    serialized_favorites = [f.serialize() for f in favorites]
+
+    response_body = {
+        "msg": f"Aqui tienes los favoritos de {user.email}",
+        "favorites": serialized_favorites
+    }
+
+    return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -6,14 +6,204 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    favorites = db.relationship('Favorite', backref='user')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorites": [fav.serialize() for fav in self.favorites]
+        }
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    climate = db.Column(db.String(80))
+    terrain = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f'<Planet {self.name}>'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'climate': self.climate,
+            'terrain': self.terrain
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(280))
+    titulo = db.Column(db.String(280))
+
+    def __repr__(self):
+        return f'<People {self.name}>'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+
+    
+    planet = db.relationship('Planet')
+    people = db.relationship('People')
+
+    def __repr__(self):
+        return f'<Favorite {self.id}>'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'planet': self.planet.serialize() if self.planet else None,
+            'people': self.people.serialize() if self.people else None
+        }
+
+
+# Relación de 1 a 1
+# Relación entre un usuario y su perfil de usuario. 
+# Cada usuario solo tiene un perfil de usuario y cada perfil de usuario pertenece a un solo usuario.
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+   
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'profile': self.profile.serialize() if self.profile else None # se accede al objeto profile para confirmar si existe o se devuelve none
+        }
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120))
+    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
+
+    member = db.relationship('Member', uselist=False) #useList se usa para establecer una relacion uno a uno
+
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'full_name': self.full_name,
+            'member_id': self.member_id,
+        }
+
+
+##---------------------------------------------------------------------------------------------------------
+
+# Relación de 1 a muchos // Modelo de tablas // Logica aplicacion
+# Relación entre una tienda y sus productos. 
+# Una tienda tiene muchos productos, que estan relacionados a una tienda
+
+class Store(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('store.id'), nullable=False)
+    store = db.relationship('Store')
+
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'store_id': self.store_id,
+            'store_name': self.store.name
+        }
+
+
+
+##---------------------------------------------------------------------------------------------------------
+
+# Relación de muchos a 1
+# Relación entre un pedido y un cliente. 
+# Muchos pedidos pueden pertenecer a un solo cliente.
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    orders = db.relationship('Order', backref='customer')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'orders': [o.serialize() for o in self.orders]
+        }
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product = db.Column(db.String(80), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'product': self.product,
+            'quantity': self.quantity,
+            'customer_id': self.customer_id
+        }
+
+
+##---------------------------------------------------------------------------------------------------------
+
+# Relación de muchos a muchos
+# Relación entre los estudiantes y los cursos. 
+# Muchos estudiantes pueden estar inscritos en muchos cursos.
+
+
+# Tabla intermedia
+inscription = db.Table('inscription',
+    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
+)
+
+# Clases de modelo
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    courses = db.relationship('Course', secondary=inscription, backref='students')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'courses': [c.name for c in self.courses]
+        }
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'students': [s.name for s in self.students]
         }
